8fcdb234c7ba9f5649b59a88ea5e24e505d312a4
dutaposco <dutaposco@gmail.com>
2026-08-03 14:31:16 +0700

    Change all resource creation buttons to standard size and label Create

--- app/Filament/Resources/CustomerInvoiceResource/Pages/ListCustomerInvoices.php
+++ app/Filament/Resources/CustomerInvoiceResource/Pages/ListCustomerInvoices.php
@@ -14,8 +14,7 @@ class ListCustomerInvoices extends ListRecords
     {
         return [
             Actions\CreateAction::make()
-                ->size(\Filament\Support\Enums\ActionSize::ExtraLarge)
-                ->extraAttributes(['class' => 'px-6 py-3 text-xl font-bold']),
+                ->label('Create'),
         ];
     }
 }
