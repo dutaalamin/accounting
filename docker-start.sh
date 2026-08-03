50ec6901b0ab9a09f863a9003b908842fdb5d085
dutaposco <dutaposco@gmail.com>
2026-08-03 15:18:20 +0700

    Ignore local sqlite database in docker builds and ensure it is created on startup

--- docker-start.sh
+++ docker-start.sh
@@ -4,6 +4,8 @@ a2dismod mpm_event || true
 a2dismod mpm_worker || true
 a2enmod mpm_prefork || true
 
+mkdir -p database
+touch database/database.sqlite
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
